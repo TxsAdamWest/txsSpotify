e74adf6cc85dfc0d7304ef6f9cc1e251671b41fa
TxsAdamWest <Twinadanz@gmail.com>
2016-08-29 00:36:07 -0500

    constructing routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component/'
+import { ROUTER_DIRECTIVES } from '@angular/router'
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html',
-    directives: [NavbarComponent]
+    directives: [ROUTER_DIRECTIVES, NavbarComponent]
 })
 export class AppComponent { }
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,15 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app.module';
+// import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+
+// platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+import { bootstrap } from '@angular/platform-browser-dynamic'
+import { appRouterProviders } from './app.routes'
+import { AppComponent } from './app.component'
+
+bootstrap(AppComponent, [appRouterProviders])
+
+
