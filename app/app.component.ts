dc03d35fa0fb6c7facf7d6df3af84f270633c792
TxsAdamWest <Twinadanz@gmail.com>
2016-08-29 00:52:58 -0500

    routes defined

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component/'
 import { ROUTER_DIRECTIVES } from '@angular/router'
+import { AboutComponent } from './components/about/about.component/'
+import { SearchComponent } from './components/search/search.component/'
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html',
-    directives: [ROUTER_DIRECTIVES, NavbarComponent]
+    directives: [ROUTER_DIRECTIVES, NavbarComponent],
+    precompile: [SearchComponent, AboutComponent]
 })
 export class AppComponent { }
