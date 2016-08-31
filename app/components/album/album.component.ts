a12aae5bf45ebf91bc6a87aad9bd34beb99a1aa5
TxsAdamWest <Twinadanz@gmail.com>
2016-08-31 03:45:18 -0500

    added images folder

--- app/components/album/album.component.ts
+++ app/components/album/album.component.ts
@@ -21,7 +21,7 @@ export class AlbumComponent {
 
 	}
 
-	msConverter(duration) {
+	msConverter(duration) { // Needed to convert time from ms to MM:SS format.
   		var minutes = Math.floor(duration / 60000);
   		var seconds = ((duration % 60000) / 1000).toFixed(0);
   		return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
