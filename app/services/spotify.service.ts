e2ccccb2f5a0a4142dde0d84c994cc943f71f74d
TxsAdamWest <Twinadanz@gmail.com>
2016-08-31 00:05:08 -0500

    create artistSearch service call

--- app/services/spotify.service.ts
+++ app/services/spotify.service.ts
@@ -5,6 +5,7 @@ import 'rxjs/add/operator/map'; // Also require observable, so we will import th
 @Injectable()
 export class SpotifyService{ // Inside here we will make our api request.
 	private searchUrl: string; // This is where we collect our search string as the user is typing.
+	private artistUrl: string;
 
 	constructor(private _http:Http) {
 
@@ -14,5 +15,11 @@ export class SpotifyService{ // Inside here we will make our api request.
 		this.searchUrl = 'https://api.spotify.com/v1/search?query=' + str + '&offset=0&limit=20&type=' + type + '&market=US'; //Following the url structure as listed in spotify's api docs.
 		return this._http.get(this.searchUrl)//_http.get() will fetch our data with the searchUrl we've created.
 			.map(res => res.json())// Then map our results into an array.
+	}
+
+	searchArtist(id: string, type='artist') {// The id from our artist object from searchArtist() where it will be concatenated into our artist Url.
+		this.searchUrl = 'https://api.spotify.com/v1/artists/' + id;
+		return this._http.get(this.artistUrl)//_http.get() will fetch our data with the artistUrl we've created.
+			.map(res => res.json())// Again, maps results into an array.
 	}
 }
