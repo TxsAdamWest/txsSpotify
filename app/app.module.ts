3ed812ff3ba33d86d542ad2d7cb4a9f161037d0a
TxsAdamWest <Twinadanz@gmail.com>
2016-08-30 23:26:52 -0500

    added visual feedback for search results to DOM

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,21 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { FormsModule } from '@angular/forms';
-
 // Testing import providers for router.
 import {appRouterProviders} from './app.routes'
-
-
-//Testing Route pathing.  
-import { SearchComponent } from './components/search/search.component'
-import { AboutComponent } from './components/about/about.component'
 
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ],
-  // Testing providers import.
-  providers: appRouterProviders
+  providers: appRouterProviders // Importing providers fixed RC4 -> RC5 migration bugs.
 })
 export class AppModule { }
